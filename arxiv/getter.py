2cae043916191ca02a1eb04539e414426bcfa416
Ion Freeman <ion@freeman.nyc>
2020-05-09 10:04:01 -0400

    Implemented Sickle
    Now only querying for 50,000 files at a time

--- arxiv/getter.py
+++ arxiv/getter.py
@@ -1,10 +1,14 @@
 import time
+
+from arxiv.sickle_impl import Sickle_Impl
+from arxiv import getLogger
+
 MAX_DELAY = 18000
 base_url = "http://export.arxiv.org/api/query?"
 import sys
-import requests # https://requests.readthedocs.io/en/master/api/
+import requests  # https://requests.readthedocs.io/en/master/api/
 import json
-#import pdftotext
+# import pdftotext
 import concurrent
 from concurrent import futures
 from concurrent.futures import Future, ThreadPoolExecutor, TimeoutError
@@ -13,6 +17,7 @@ import os
 import re
 from urllib import parse
 from lxml import etree
+from typing import List
 
 """
 https://arxiv.org/help/api/tou
@@ -23,19 +28,7 @@ THe API Services Gateway has not been delivered, so we're using the arXiv API wi
 """
 
 # BEGIN LOGGER SETUP
-import logging
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-console = logging.StreamHandler()
-console.setFormatter(formatter)
-console.setLevel(logging.INFO)
-logger.addHandler(console)
-logfile = logging.FileHandler(f"{__name__}.log")
-logfile.setFormatter(formatter)
-logfile.setLevel(logging.DEBUG)  # conf (ie TODO move to configuration)
-logger.addHandler(logfile)
+logger = getLogger(__name__)
 # END LOGGER SETUP
 logger.info("Logger initialized")
 # SET UP EXECUTOR
@@ -46,21 +39,25 @@ print("; ".join(sys.argv))
 
 topic = sys.argv[1]
 
-max_records = int(sys.argv[2])                                   #conf
+max_records = int(sys.argv[2])  # conf
 
 # hard code the forty arXiv CS categories; they're unlikely to change before the whole system's replaced
-arxiv_categories = {"cs":['AI', 'AR', 'CC', 'CE', 'CG', 'CL', 'CR', 'CV', 'CY', 'DB', 'DC', 'DL', 'DM', 'DS', 'ET', 'FL', 'GL', 'GR', 'GT', 'HC', 'IR', 'IT', 'LG', 'LO', 'MA', 'MM', 'MS', 'NA', 'NE', 'NI', 'OH', 'OS', 'PF', 'PL', 'RO', 'SC', 'SD', 'SE', 'SI', 'SY']}
-arxiv_categories_querystring = '+OR+'.join([f"cat:{key}.{val}" for key in arxiv_categories for val in arxiv_categories[key]])
+arxiv_categories = {
+    "cs": ['AI', 'AR', 'CC', 'CE', 'CG', 'CL', 'CR', 'CV', 'CY', 'DB', 'DC', 'DL', 'DM', 'DS', 'ET', 'FL', 'GL', 'GR',
+           'GT', 'HC', 'IR', 'IT', 'LG', 'LO', 'MA', 'MM', 'MS', 'NA', 'NE', 'NI', 'OH', 'OS', 'PF', 'PL', 'RO', 'SC',
+           'SD', 'SE', 'SI', 'SY']}
+arxiv_categories_querystring = '+OR+'.join(
+    [f"cat:{key}.{val}" for key in arxiv_categories for val in arxiv_categories[key]])
 
 # determine if downloaded bytes are marked as a PDF document
 pdf_test = lambda response_content: response_content[:4] == b'%PDF'
-ns={'arxiv': 'http://arxiv.org/schemas/atom'
+ns = {'arxiv': 'http://arxiv.org/schemas/atom'
     , 'atom': 'http://www.w3.org/2005/Atom'
     , 'html': 'http://www.w3.org/1999/xhtml'
     , 'opensearch': 'http://a9.com/-/spec/opensearch/1.1/'}
 
 
-def to_ordinal(cardinal:int):
+def to_ordinal(cardinal: int):
     cardinal_str = str(cardinal)
     last_digit = cardinal_str[-1]
     if last_digit in ('1', '2', '3') and (len(cardinal_str) == 1 or cardinal_str[-2] != "1"):
@@ -68,7 +65,7 @@ def to_ordinal(cardinal:int):
             append = 'st'
         elif last_digit == '2':
             append = 'nd'
-        else: # last_digit == '3'
+        else:  # last_digit == '3'
             append = 'rd'
     else:
         append = 'th'
@@ -81,6 +78,7 @@ def str2dict(tokens: str, field_delimiter=';', pair_delimiter='='):
                [token.strip() for token in tokens.split(field_delimiter)]]:
         retval[nv[0]] = nv[1]
 
+
 def qualify_entry(entry: etree._Element, ns: dict):
     '''
     :param entry: an 'entry' child of the arXiv feed element
@@ -92,7 +90,8 @@ def qualify_entry(entry: etree._Element, ns: dict):
     whether = whether and entry.find('atom:link[@type="application/pdf"]', ns) is not None
     return whether
 
-def url_to_file_name(url:str):
+
+def url_to_file_name(url: str):
     '''
     Take off the protocol and turn the dots and slashes to underscores. This will preserve uniqueness in this set,
     but not generally. Don't let this code get out ;)
@@ -101,7 +100,8 @@ def url_to_file_name(url:str):
     '''
     return url.split('://')[-1].replace('.', '_').replace('/', '_')
 
-def no_pdf(pdf_bytes:bytes, ns:dict=ns):
+
+def no_pdf(pdf_bytes: bytes, ns: dict = ns):
     """
     Determines whether arXiv has a 'no pdf' page set up for this URL
     :param pdf_bytes: the non-pdf content returned from the pdf_url
@@ -113,7 +113,7 @@ def no_pdf(pdf_bytes:bytes, ns:dict=ns):
         html = etree.fromstring(pdf_bytes, etree.HTMLParser())
         title_elements = html.xpath('/html/head/title')
         if title_elements and len(title_elements):
-            title:str = title_elements[0].text
+            title: str = title_elements[0].text
             if title:
                 return title.startswith('No PDF')
         else:
@@ -123,7 +123,7 @@ def no_pdf(pdf_bytes:bytes, ns:dict=ns):
     return False
 
 
-def detect_refresh_request(pdf_bytes:bytes, ns:dict = ns):
+def detect_refresh_request(pdf_bytes: bytes, ns: dict = ns):
     """
     Sometimes, arXiv asks you to wait ten seconds and try again
     :return:
@@ -132,7 +132,7 @@ def detect_refresh_request(pdf_bytes:bytes, ns:dict = ns):
         html = etree.fromstring(pdf_bytes, etree.HTMLParser())
         meta_elements = html.xpath('/html/head/meta[@http-equiv="refresh"]')
         if meta_elements and len(meta_elements):
-            meta_element_attrib:dict = meta_elements[0].attrib
+            meta_element_attrib: dict = meta_elements[0].attrib
             refresh_delay = int(meta_element_attrib.get('content', "10"))
             return refresh_delay
         logger.debug(f"Non-refresh response detected \n{pdf_bytes.decode('UTF-8')}")
@@ -141,7 +141,7 @@ def detect_refresh_request(pdf_bytes:bytes, ns:dict = ns):
     return False
 
 
-def link_from_entry(entry:etree._Element):
+def link_from_entry(entry: etree._Element):
     """
     from https://export.arxiv.org/denied.html
     For automated programmatic harvesting
@@ -150,21 +150,30 @@ def link_from_entry(entry:etree._Element):
     :param entry: extracted journal entry from atom feed
     :return: pdf download link
     """
-    raw_pdf_link:str = entry.find('atom:link[@type="application/pdf"]', ns).attrib['href']
-    export_pdf_link:str = raw_pdf_link.replace("//arxiv.org", "//export.arxiv.org")
+    raw_pdf_link: str = entry.find('atom:link[@type="application/pdf"]', ns).attrib['href']
+    export_pdf_link: str = raw_pdf_link.replace("//arxiv.org", "//export.arxiv.org")
     return export_pdf_link
 
-def query_arXiv(base_url = base_url, max_records = max_records, batch_number = 0, topic=topic, delay_s = 3):
+
+# TODO change to a POST request limiting to the passed IDs
+def query_arXiv(id_batch: List[str], base_url=base_url, max_records=max_records, batch_number=0, topic=topic, year=None,
+                delay_s=3):
     """
     reaches out to legacy arXiv query service
     NOTE The sort order is hard coded here
     @:return byte array response from http call
     """
     # requests.get('http://export.arxiv.org/api/query?max_results=200&start=0&search_query=cat:cs+AND+all:computing&sort_by=lastUpdatedDate&sort_order=descending')
-    time.sleep(delay_s)
     query_text = f'({arxiv_categories_querystring})+AND+all:{topic}'
-    query = f"{base_url}max_results={max_records}&start={batch_number * max_records}&search_query={query_text}&sort_by=lastUpdatedDate&sort_order=descending"
-    query_response = requests.get(query)
+    logger.info(f"In {delay_s} seconds, attempting metadata query {batch_number}")
+    time.sleep(delay_s)
+    data: dict = dict(max_results=max_records
+                      , start=batch_number * max_records
+                      , search_query=query_text
+                      , sort_by='lastUpdatedDate'
+                      , sort_order='descending'
+                      , id_list=','.join(id_batch))
+    query_response = requests.post(base_url, data)
     if query_response.status_code == 200:
         return query_response.content
     else:
@@ -172,7 +181,7 @@ def query_arXiv(base_url = base_url, max_records = max_records, batch_number = 0
         raise Exception(f"failed query {batch_number} {max_records} {topic}")
 
 
-def get_pdf_links(topic, batch_number = 0, max_records = max_records, init_delay_s = 3, ns:dict = ns):
+def get_pdf_links(id_batch: List[str], topic, batch_number=0, max_records=max_records, init_delay_s=3, ns: dict = ns):
     """
     arXiv API user's manual: https://arxiv.org/help/api/user-manual
     :return (whether arXiv returned as many entried as requested, filtered list of pdf download links)
@@ -181,14 +190,15 @@ def get_pdf_links(topic, batch_number = 0, max_records = max_records, init_delay
     last_delay = 0
     current_delay = init_delay_s
     retry = True
-    while(retry and current_delay < MAX_DELAY):
+    while (retry and current_delay < MAX_DELAY):
         try:
             # We can't use requests.get's query string builder as it url encodes colons and spaces, which arXiv does not permit
-            response_bytes = query_arXiv(base_url, max_records, batch_number, topic, current_delay)
+            response_bytes = query_arXiv(id_batch, base_url, max_records, batch_number, topic, current_delay)
             # 4. Find sections with something like 'grep -E '^\s*((I?(X(I?V)?|V)I?)|I)I{0,2}\.\s+[A-Z]\s?[A-Z]+' computing/*.txt'
             # 5. Store each section in a subfolder named for the section title
-            response_tree:etree._Element = etree.fromstring(response_bytes)
-            pdf_links = [link_from_entry(entry) for entry in response_tree.getiterator('{http://www.w3.org/2005/Atom}entry') if qualify_entry(entry, ns)]
+            response_tree: etree._Element = etree.fromstring(response_bytes)
+            pdf_links = [link_from_entry(entry) for entry in
+                         response_tree.getiterator('{http://www.w3.org/2005/Atom}entry') if qualify_entry(entry, ns)]
             # Calculate the total entries; this will tell you if you're on the last page
             records_returned = len(response_tree.findall('atom:entry', ns))
             (total_results, start_index, items_per_page) = map(
@@ -201,7 +211,7 @@ def get_pdf_links(topic, batch_number = 0, max_records = max_records, init_delay
                 last_delay = current_delay
                 current_delay = next_delay
                 logger.info(f'''query return {records_returned} total entries, whereas we have only found {start_index}
-of an expected {total_results}. Trying again in {current_delay} seconds''')
+of an expected {total_results}.''')
             else:
                 logger.info(f'query returned {records_returned} total entries and {len(pdf_links)} qualified entries')
                 return (records_returned == max_records, pdf_links)
@@ -211,15 +221,20 @@ of an expected {total_results}. Trying again in {current_delay} seconds''')
     # We kept trying, but we didn't make it
     return (False, None)
 
-def yield_pdf_links(query_text:str, max_records:int):
+
+def yield_pdf_links(id_batch: List[str], query_text: str, max_records: int):
     batch_index = 0
-    while more_batches:= True:
-        (more_batches, pdf_links) = get_pdf_links(query_text, batch_index, max_records)
+    logger.debug("reset batch id")
+    # TODO catch the big-topic exception if the number is over 50,000 and break the query up by year
+    more_batches = True
+    while more_batches == True:
+        (more_batches, pdf_links) = get_pdf_links(id_batch, query_text, batch_index, max_records)
         yield pdf_links
+        logger.debug(f"completed batch {batch_index}")
         batch_index += 1
 
 
-def download_pdf(target_dir:str, pdf_url:str):
+def download_pdf(target_dir: str, pdf_url: str):
     """
     :param target_dir: directory in which to store the downloaded article in Adobe's portable document format
     :param pdf_url: link to pdf
@@ -234,8 +249,8 @@ def download_pdf(target_dir:str, pdf_url:str):
     # retrieve the pdf file
     # set up a fibonacci backoff
     last_backoff = 0
-    backoff = 1 # conf
-    for trial in range(20): #conf
+    backoff = 1  # conf
+    for trial in range(20):  # conf
         # Fibonacci elements and cumulative wait times in seconds and hours
         #   1   1   2   3   5   8   13      21      34      55      89      144     233     377     610     987         1597            2584            4181            6765
         #   1   2   4   7   12  20  33      54      88      143     232     376     609     986     1596    2583        4180            6764            10945           17020
@@ -268,28 +283,37 @@ def download_pdf(target_dir:str, pdf_url:str):
             logger.debug(f"{pdf_url}: created {pdf_path}")
         return pdf_path
 
-def download_pdfs(topic:str, max_records:int):
+
+def download_pdfs(id_batch: List[str], topic: str, max_records: int):
     topic_dir = parse.quote_plus(topic)
     os.makedirs(topic_dir, exist_ok=True)
-    for pdf_links in yield_pdf_links(topic, max_records):
+    for pdf_links in yield_pdf_links(id_batch, topic, max_records):
         for pdf_link in pdf_links:
             yield executor.submit(download_pdf, topic, pdf_link)
 
 
 def main():
-    download_path_futures = download_pdfs(topic, max_records)
-    for download_path in download_path_futures:
-        if download_path.result():
-            logger.debug(f"Saved PDF to {download_path.result()}")
+    for id_batch in Sickle_Impl().get_batched_ids(50000, 'cs'):
+        logger.info(f"starting batch from {id_batch[0]} to {id_batch[-1]}")
+        download_path_futures = download_pdfs(id_batch, topic, max_records)
+        for download_path in download_path_futures:
+            try:
+                if download_path.result(3600):
+                    logger.debug(f"Saved PDF to {download_path.result()}")
+            except futures.TimeoutError as te:
+                logger.warning(f"{download_path} timed out")
+
 
 if __name__ == "__main__":
     # execute only if run as a script
     main()
+
 
 class getter:
     """ write a parallelized enqueuer to execute the API query in batches and send the article numbers to a list
     parallelized dequeuers will take batches of them off the list and sent them to Google cloud functions which will
     retrieve and store the PDFs
     """
+
     def enqueue(self):
         pass
--- arxiv/sickle_impl.py
+++ arxiv/sickle_impl.py
@@ -1,20 +1,36 @@
 import sickle
 from sickle.iterator import OAIItemIterator
+import logging
+
+from arxiv import getLogger
+
+logger = getLogger(__name__)
 
 
-arxiv:OAIItemIterator = sickle.Sickle("http://export.arxiv.org/oai2", iterator=OAIItemIterator)
-# for metadata_format in ['oai_dc', 'arXiv', 'arXivRaw', 'arXivOld']:
-metadata_format = 'arXivRaw'
-print(f"*** {metadata_format} ***")
-counter = 0
-for item in arxiv.ListRecords(metadataPrefix = metadata_format, set = 'cs'):
-    counter += 1
-    print(item.header.identifier)
-    print(item.raw)
-    print(item.metadata['id'])
-    print()
-    print()
-    if counter > 10:
-        break
+class Sickle_Impl:
+    def __init__(self, oai_url="http://export.arxiv.org/oai2", metadata_format='arXivRaw'):
+        self.metadata_format = metadata_format
+        self.arxiv: OAIItemIterator = sickle.Sickle(oai_url, iterator=OAIItemIterator)
+        print(f"*** extracting metadata from {oai_url} in {metadata_format} format ***")
 
-print(arxiv)
+    def get_ids(self, set: str = 'cs'):
+        counter = 0
+        for item in self.arxiv.ListRecords(metadataPrefix=self.metadata_format, set=set):
+            counter += 1
+            # logger.trace(f"item {counter} is {item.header.identifier}")
+            ids = item.metadata['id']
+            if len(ids) > 0:
+                yield ids[0]
+            if len(ids) > 1:
+                logger.warning(f"item {counter} had multiple ids: {';'.join(ids)}")
+
+    def get_batched_ids(self, batch_size=50000, set='cs'):
+        current_batch = list()
+        for id in self.get_ids(set):
+            current_batch.append(id)
+            current_batch_size = len(current_batch)
+            if 0 == current_batch_size % 200:
+                logger.debug(f"Harvested {current_batch_size} ids")
+            if current_batch_size >= batch_size:
+                yield current_batch
+                current_batch = list()
